7425c2f3b3500690f91ec5fdfd89ef84b9cd0889
Tomas-Hall <tomas.hall@live.co.uk>
2018-12-12 00:58:53 +0000

    Create 99 bottles of beer on the wall.ps1
    meant Python.
    Powershell equivalent script added for 99 bottles.
    
    used recursive function call because why not

---
+++ Powershell/99 bottles of beer on the wall.ps1
@@ -0,0 +1,14 @@
+function Get-BottleString($bottles) {
+    return "$bottles " + $(if ($bottles -ne 1) { "bottles" } else { "bottle" })
+}
+function Push-BottleOff($bottles) {
+    if($bottles -le 0) { return }
+    $str = Get-BottleString -bottles $bottles
+    Write-Host "$str of beer on the wall, $str of beer."
+    --$bottles
+    $str = Get-BottleString -bottles $bottles
+    Write-Host "Take one down and pass it around, $str of beer on the wall. `r`n"
+    Push-BottleOff($bottles)
+    return
+}
+Push-BottleOff -bottles 3
